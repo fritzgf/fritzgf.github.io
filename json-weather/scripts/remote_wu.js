4043b75995dd3999830cf06234ffefbd2f39fcbf
fritzgf <fgfevrier@gmail.com>
2017-11-21 22:53:21 -0700

    add new comments

--- json-weather/scripts/remote_wu.js
+++ json-weather/scripts/remote_wu.js
@@ -65,6 +65,7 @@ $(function () {
     function toTitleCase(str) {
         return str.replace(/\w+/g, function (txt) {
             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            console.log(data);
         });
     }
 });
